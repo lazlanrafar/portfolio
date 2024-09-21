1e580f090722cd5ef85236e0ee91d8aa339c3044
lazlanrafar <lazlanrafar@gmail.com>
2024-09-21 13:24:45 +0800

    feat(project): fetch all project from microcms

--- src/components/molecules/project-card.tsx
+++ src/components/molecules/project-card.tsx
@@ -1,33 +1,63 @@
 import { Project } from "@/types";
+import { IMicrocmsProject } from "@/types/microcms-response";
 import Image from "next/image";
 import React from "react";
 
-export default function ProjectCard({ project }: { project: Project }) {
+export default function ProjectCard({
+  project,
+}: {
+  project: IMicrocmsProject;
+}) {
   return (
-    <a href={project.project_url || "#"} target="_blank">
-      <article className="group cursor-pointer border rounded-md overflow-hidden">
-        <figure className="relative aspect-video rounded-t-md">
+    <a
+      href={project.project_url || "#"}
+      target={project.project_url ? "_blank" : "_self"}
+      rel="noopener noreferrer"
+    >
+      <article className="group cursor-pointer w-full sm:w-[300px] xl:w-[370px] 2xl:w-[336px] mb-10 mx-1">
+        <figure className="relative mb-8">
           <Image
-            className="object-cover object-center group-hover:scale-105 transition-transform duration-500"
-            src={project.thumbnail || "/placeholder.png"}
-            blurDataURL={project.thumbnail || "/placeholder.png"}
+            className="group-hover:rotate-3 group-hover:scale-105 transition-transform"
+            src={project.thumbnail.url || "/placeholder.png"}
+            blurDataURL={project.thumbnail.url || "/placeholder.png"}
             placeholder="blur"
             quality={10}
-            sizes="100%"
-            fill
-            alt={`${project.name} thumbnail`}
+            width={768}
+            height={640}
+            alt={`${project.title} thumbnail`}
           />
-          <div className="w-full h-full absolute top-0 left-0 z-30 flex items-center justify-center bg-background/80 backdrop-blur-sm overflow-hidden group-hover:opacity-0 transition-opacity duration-500">
-            <p className="text-xl italic font-semibold uppercase p-3 text-center">
-              {project.name}
-            </p>
+
+          <div className="absolute -left-5 -bottom-5 bg-background h-[90px] w-[90px] grid place-items-center rounded-full border text-xs text-center">
+            _Preview <br />
+            プレビュー
           </div>
         </figure>
-        <div className="p-3 text-sm text-muted-foreground text-start">
-          {project.description.length > 100
-            ? `${project.description.substring(0, 100)}...`
-            : project.description}
+        <div className="mt-3">
+          <p className="text-sm truncate mb-1">{project.title}</p>
+
+          <p className="text-xs text-muted-foreground hover:underline">
+            {project.project_url ? (
+              <a
+                href={project.project_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {project.project_url}
+              </a>
+            ) : (
+              "No project URL"
+            )}
+          </p>
         </div>
+        {/* <div className="p-3 text-sm text-muted-foreground text-start">
+          <p
+            dangerouslySetInnerHTML={{
+              __html: project.description
+                ? project.description.slice(0, 65)
+                : "",
+            }}
+          ></p>
+        </div> */}
       </article>
     </a>
   );
--- src/lib/env.ts
+++
@@ -1,5 +0,0 @@
-export const ENV = {
-  NEXT_PUBLIC_WHOAMI_URL:
-    process.env.NEXT_PUBLIC_WHOAMI_URL || "localhost:3000",
-  NEXT_PUBLIC_WHOAMI_ASSETS_URL: process.env.NEXT_PUBLIC_WHOAMI_ASSETS_URL,
-};
